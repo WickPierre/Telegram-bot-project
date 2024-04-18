c6f96119a2bb33bd6bd404cf2ae0610a7dc5a75c
petrlutkin <lutkinpeter@gmail.com>
2024-04-18 20:36:47 +0300

    Hid and modified (again) bot token

--- main.py
+++ main.py
@@ -1,14 +1,21 @@
 import os
+import json
 import logging
 from telegram.ext import Application, MessageHandler, filters
-from config import BOT_TOKEN
+from dotenv import load_dotenv
+from googleapiclient.discovery import build
+
+load_dotenv()
+API_KEY = os.getenv('API_KEY')
+BOT_TOKEN = os.getenv('BOT_TOKEN')
+youtube = build('youtube', 'v3', developerKey=API_KEY)
 
 logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.DEBUG)
 logger = logging.getLogger(__name__)
 
 
 async def echo(update, context):
-    await update.message.reply_text("Я получил сообщение " + update.message.text)
+    await update.message.reply_text(youtube.search().list(part="snippet", q="mrbeast", maxResults=1).execute())
 
 
 def main():
